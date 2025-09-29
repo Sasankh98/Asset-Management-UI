139945c0e2e6122493042a8d5d471689e13be0ea
Sasankh <sasankh1805@gmail.com>
2025-09-30 01:54:02 +0530

    ..

--- src/AssetManagement/Pages/Stocks/StocksForms/index.ts
+++ src/AssetManagement/Pages/Stocks/StocksForms/index.ts
@@ -1,3 +1,3 @@
-export { default as CreateStocks } from './createForm/CreateStocks'; 
-export { default as EditStocks } from './editForm/EditStocks'; 
-export { default as InfoStocks } from './infoForm/InfoStocks'; 
+// export { default as CreateStocks } from './createForm/CreateStocks'; 
+// export { default as EditStocks } from './editForm/EditStocks'; 
+// export { default as InfoStocks } from './infoForm/InfoStocks'; 
