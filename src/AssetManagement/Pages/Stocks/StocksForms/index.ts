b7640e0b93f29472f15c89c9a92452ad1a410969
sasankh <sasankh1805@gmail.com>
2026-05-10 14:46:01 +0530

    removed unused files

--- src/AssetManagement/Pages/Stocks/StocksForms/index.ts
+++
@@ -1,3 +0,0 @@
-// export { default as CreateStocks } from './createForm/CreateStocks'; 
-// export { default as EditStocks } from './editForm/EditStocks'; 
-// export { default as InfoStocks } from './infoForm/InfoStocks'; 
